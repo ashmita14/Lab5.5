cc308e122609b107022983d3bb9425d46ac42838
ashmita14 <42172528+ashmita14@users.noreply.github.com>
2018-09-06 19:25:17 +0530

    Add files via upload

---
+++ lab5.5_q6.cpp
@@ -0,0 +1,31 @@
+//Mirrored Rhombus Star Pattern
+
+//including libraries
+#include<iostream>
+using namespace std;
+//declaring main function
+int main(){
+	//declaring variables
+	int n, i, j, k;
+	//asking user for size of pattern
+	cout <<"Enter the size of the pattern."<<endl;
+	//accepting value
+	cin >>n;
+	//running loop to print pattern
+	for(i=0; i<n; i++){
+		//printing spaces
+		for(j=0; j<i; j++){
+			cout<<" ";
+		}
+		//printing stars
+		for(k=0; k<=i; k++){
+			cout <<"*";
+		}
+		//changing line
+		cout<<endl;
+	}
+	//returning integer value to main function
+	return 0;
+}
+
+
