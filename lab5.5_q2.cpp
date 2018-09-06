84fbd7d2f0f5b10cab8c3e52438d3e15771e2bff
ashmita14 <42172528+ashmita14@users.noreply.github.com>
2018-09-06 18:31:09 +0530

    Add files via upload

---
+++ lab5.5_q2.cpp
@@ -0,0 +1,43 @@
+//Hollow Star Pattern
+
+//including libraries
+#include<iostream>
+using namespace std;
+//declaring main function
+int main(){
+	//declaring varia
+	int i, j, k, n;
+	//asking user for size of pattern
+	cout <<"Enter the size of the pattern."<<endl;
+	//accepting value
+	cin >>n;
+	//running loop to generate pattern
+	for(i=0; i<n; i++){
+		//checking value of i
+		if(i==0 || i==(n-1)){
+			//loop for printing stars of 1st and last line
+			for(j=0; j<n; j++){
+				//printing stars
+				cout <<"*";
+			}
+			//changing line
+			cout<<endl;
+		}
+		//else condition
+		else{
+			//print star
+			cout <<"*";
+			//loop to print space
+			for(k=1; k<n; k++){
+				//printing space
+				cout <<" ";
+			}
+			//printing star
+			cout <<"*";
+			//changing line
+			cout<<endl;
+		}
+	}
+	//returning integer value to main function
+	return 0;
+}
